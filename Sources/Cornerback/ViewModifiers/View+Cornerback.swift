6df378ee299da07353f4e7a112b08d8f708106c8
Adolfo <adolfo.vera@globant.com>
2023-05-19 12:47:05 +0200

    Fix access control for ViewModifier and App

--- Sources/Cornerback/ViewModifiers/View+Cornerback.swift
+++ Sources/Cornerback/ViewModifiers/View+Cornerback.swift
@@ -15,7 +15,7 @@ public typealias CornerbackModifierClosure = (Cornerback) -> Void
 
 @available(iOS 13, macOS 10.15, *)
 extension View {
-    func networkRules(closure: CornerbackModifierClosure) -> some View {
+    public func networkRules(closure: CornerbackModifierClosure) -> some View {
         let modifier = CornerbackModifier(closure: closure)
         
         return ModifiedContent(content: self, modifier: modifier)
@@ -36,7 +36,7 @@ struct CornerbackModifier: ViewModifier {
 
 @available(iOS 14.0, macOS 11.0, *)
 extension App {
-    func networkRules(performClosure: CornerbackModifierClosure) -> Self {
+    public func networkRules(performClosure: CornerbackModifierClosure) -> Self {
         performClosure(Cornerback.shared)
         
         return self
