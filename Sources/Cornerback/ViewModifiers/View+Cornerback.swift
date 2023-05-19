d6309a0d2e7cd069f13db4015eee264a70e1dbb0
Adolfo <adolfo.vera@globant.com>
2023-05-19 12:11:53 +0200

    Support for SwiftUI using a ViewModifier

---
+++ Sources/Cornerback/ViewModifiers/View+Cornerback.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  
+//
+//  Created by Adolfo Vera Blasco on 19/5/23.
+//
+
+import Foundation
+#if canImport(SwiftUI)
+import SwiftUI
+#endif
+
+@available(iOS 13, macOS 10.15, *)
+public typealias CornerbackModifierClosure = (Cornerback) -> Void
+
+@available(iOS 13, macOS 10.15, *)
+extension View {
+    func registerDependencies(closure: CornerbackModifierClosure) -> some View {
+        let modifier = CornerbackModifier(closure: closure)
+        
+        return ModifiedContent(content: self, modifier: modifier)
+    }
+}
+
+
+@available(iOS 13, macOS 10.15, *)
+struct CornerbackModifier: ViewModifier {
+    init(closure: CornerbackModifierClosure) {
+        closure(Cornerback.shared)
+    }
+    
+    public func body(content: Content) -> some View {
+        content
+    }
+}
+
